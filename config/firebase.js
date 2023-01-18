cedb7314719e31fb5f6cd10afaeff908502f51f1
Olivia Buckland <92247079+OBuckland@users.noreply.github.com>
2023-01-18 12:08:21 +1100

    first commit

---
+++ config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAQ9UuPHJqpUZdZDRds4PvfG7ojuE65zdY",
+  authDomain: "finance-tracker-database.firebaseapp.com",
+  projectId: "finance-tracker-database",
+  storageBucket: "finance-tracker-database.appspot.com",
+  messagingSenderId: "997134907579",
+  appId: "1:997134907579:web:7474721fa23eb124e4519e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
